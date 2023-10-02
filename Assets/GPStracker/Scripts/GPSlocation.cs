f4e264f6cf3a02875337022fa0dbaca309af56e0
Michaela Ř <majkeltcz@gmail.com>
2023-10-02 19:41:27 +0200

    lokace

--- Assets/GPStracker/Scripts/GPSlocation.cs
+++ Assets/GPStracker/Scripts/GPSlocation.cs
@@ -1,70 +1,78 @@
+
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Android;
 
-public class GPSlocation : MonoBehaviour
+public class GPS : MonoBehaviour
 {
-    public Text GPSStatus;
-    public Text latitudeValue;
-    public Text longitudeValue;
-    public Text altitudeValue;
-    public Text horizontalAccuracyValue;
-    public Text timestampValue;
+   public Text  latitude ;
+    public Text      longitude ;
+    public Text     altitude;
+    public Text       horizontalAccuracy ;
+    public Text      timestamp;
+    public Text status;
 
+    public bool isUpdating;
 
-
-    void Start()
+    private void Update()   {
+        if (!isUpdating)
+        {
+            StartCoroutine(GetLocation());
+            isUpdating = !isUpdating;
+        }
+    }
+    IEnumerator GetLocation()
     {
-        StartCoroutine(GPSLoc());
+        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+            Permission.RequestUserPermission(Permission.CoarseLocation);
+        }
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+            yield return new WaitForSeconds(10);
+
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 10;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)  {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)  {
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)    {
+            yield break;
+        }
+        else{
+            latitude.text = Input.location.lastData.latitude.ToString();
+            longitude.text = Input.location.lastData.longitude.ToString();
+            altitude.text = Input.location.lastData.altitude+100f.ToString();
+            horizontalAccuracy.text =  Input.location.lastData.horizontalAccuracy.ToString();
+            timestamp.text =  Input.location.lastData.timestamp.ToString();
+
+             
+            if (Input.location.lastData.horizontalAccuracy > 100) {
+                status.text = "riziko špatné přesnosti GPS";
+            }
+            else {
+                status.text = " ";
+            }
+
+
+
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        isUpdating = !isUpdating;
+        Input.location.Stop();
     }
-
- IEnumerator GPSLoc() {
-
-    if(!Input.location.isEnabledByUser) 
-        yield break;
-
-    Input.location.Start();
-
-    int maxWait = 20;
-    while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)    {
-        yield return new WaitForSeconds(1);
-        maxWait--;
-    }
-
-if (maxWait < 1) {
-    GPSStatus.text = "Time out";
-    yield break;
-}
-if (Input.location.status == LocationServiceStatus.Failed) {
-        GPSStatus.text = "Unable to determinate";
-
-    yield break;
-}
-else {
-    GPSStatus.text = "Running";
-    InvokeRepeating("UpdateGPSDATA",0.5f,1f);
-
-}
- }
-
-
-private void UpdateGPSDATE() {
-    if(Input.location.status == LocationServiceStatus.Running) {
-    GPSStatus.text = "Running";
-    latitudeValue.text = Input.location.lastData.latitude.ToString();
-    longitudeValue.text = Input.location.lastData.longitude.ToString();
-    altitudeValue.text = Input.location.lastData.altitude.ToString();
-        horizontalAccuracyValue.text = Input.location.lastData.horizontalAccuracy.ToString();
-        timestampValue.text = Input.location.lastData.timestamp.ToString();
-
-
-
-    }
-    else {
-    GPSStatus.text = "Running";
-
-    }
-}
-
 }
