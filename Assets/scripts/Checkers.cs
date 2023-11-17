7068c10d8529d319419dcfb6f2ee5f721a9dc69b
Michaela Ř <majkeltcz@gmail.com>
2023-11-17 13:34:18 +0100

    checkers - rules (working)

--- Assets/scripts/Checkers.cs
+++ Assets/scripts/Checkers.cs
@@ -13,13 +13,13 @@ public Text test;
 public static string positionName;
 string nowPosPlace;
 int value;
-string tempPos;
+string turn = "red";
 private List<Transform> blackSquares = new List<Transform>();
 private List<Transform> redPieces = new List<Transform>();
 private List<Transform> blackPieces = new List<Transform>();
 
 
-private string[] rules = {"1", "1", "1", "1", "1", "1", "1","1","1",};
+private string[] rules = {"1", "1", "1", "1", "1", "1", "1","1","1","1"};
 
 
 
@@ -37,29 +37,22 @@ for(int i = 0; i < board.transform.childCount;i++) {
            blackPieces.Add(board.transform.GetChild(i));
     }
         }
-
-
 }
-
-
-
-
-
-
-
-
-public void getPosition() {    
+public void getPosition() {
+            
 for(int i = 0; i <blackSquares.Count;i++) {
         if(blackSquares[i].transform.position == piece.transform.position) {  
            nowPosPlace = blackSquares[i].name;
+        test.text =nameOf + " " + nowPosPlace;
     }
         }
 }
-
-bool CheckSameCollision(List<Transform> list) {
-        
+bool CheckSameCollision(List<Transform> list) {       
 for(int i = 0; i <list.Count;i++) {
-        if(list[i].transform.position == board.transform.Find(positionName).transform.position) return false;
+        if(list[i].transform.position == board.transform.Find(positionName).transform.position) {
+        test.text = "Nelze Pohnout";
+                return false;
+        }
         }
 return true;
 }
@@ -67,6 +60,7 @@ return true;
 bool CheckCollision(List<Transform> list, string checkPosition) {
 for(int i = 0;i < list.Count;i++) {
           if(list[i].transform.position == board.transform.Find(checkPosition).transform.position) {  
+                if(list[i].transform.position != board.transform.Find(positionName).transform.position) {
                    list[i].gameObject.SetActive(false);  
                    list[i].transform.position = new Vector3(0, -10, 5);
                    list.Remove(list[i]);   
@@ -74,51 +68,65 @@ for(int i = 0;i < list.Count;i++) {
                         return true;
 }
 }
+}
 return false;
 }
-
-
-
-
-
-
 
  int toInt(char c) {
    return c - '0';
 }
 bool MoveRules(string piece) {
 if(piece.Contains("redPiece")) {
-                 
-                 if  (CheckCollision(blackPieces,rules[0])) {
-                if(rules[3] == positionName) return true;
-             }
-        else if  (CheckCollision(blackPieces,rules[2])) {
-                if(rules[4] == positionName) return true;
-             } 
-        if(toInt(nowPosPlace[0]) % 2 == 0 || (toInt(nowPosPlace[0]) == 0)) {
-                if( rules[0]== positionName || rules[2] == positionName)  return true;
+        
+         if(toInt(nowPosPlace[0]) % 2 == 0 || (toInt(nowPosPlace[0]) == 0)) {
+                  if(rules[3] == positionName) {
+               if(CheckCollision(blackPieces,rules[1])) return true;
+             }  
+                 if(rules[4] == positionName) {
+               if(CheckCollision(blackPieces,rules[0])) return true;
+             }    
+
+                if( rules[0]== positionName || rules[1] == positionName)  return true;
         }
         else {
-                if( rules[0]== positionName || rules[1] == positionName) return true;
+                if( rules[0]== positionName || rules[2] == positionName) return true;
+        
+        if(rules[3] == positionName) {
+               if(CheckCollision(blackPieces,rules[0])) return true;
+             }
+         if(rules[4] == positionName) {
+                if(CheckCollision(blackPieces,rules[2])) return true;
+             } 
+        
         }
 return false;
 }
 
+if(piece.Contains("blackPiece")) {
+        if(toInt(nowPosPlace[0]) % 2 == 0) {
+                    if(rules[9] == positionName) {
+               if(CheckCollision(redPieces,rules[5])) return true;
+             }  
+                 if(rules[8] == positionName) {
+               if(CheckCollision(redPieces,rules[6])) return true;
+             }
 
+                if( rules[5]== positionName || rules[6] == positionName)  return true;
+        }
+        else {
+        if(rules[9] == positionName) {
+               if(CheckCollision(redPieces,rules[7])) return true;
+             }  
+        if(rules[8] == positionName) {
+               if(CheckCollision(redPieces,rules[5])) return true;
+             }
 
-
-
-
-
-
+                if(rules[5] == positionName || rules[7] == positionName) return true;
+        }
 return false;
 }
-
-
-
-
-
-
+return false;
+}
 
 void setRules() {
 //redPieces
@@ -129,18 +137,13 @@ rules[2] = (toInt(nowPosPlace[0]) + 1).ToString() + "blackBoard" + (toInt(nowPos
 rules[3] = (toInt(nowPosPlace[0]) + 2).ToString() + "blackBoard" + (toInt(nowPosPlace[nowPosPlace.Length - 1]) - 1).ToString();
 rules[4] = (toInt(nowPosPlace[0]) + 2).ToString() + "blackBoard" + (toInt(nowPosPlace[nowPosPlace.Length - 1]) + 1).ToString();
 
-
-
 // blackPieces rules
 rules[5] = (toInt(nowPosPlace[0]) - 1).ToString() + "blackBoard" + (nowPosPlace[nowPosPlace.Length - 1]);
 rules[6] = (toInt(nowPosPlace[0]) - 1).ToString() + "blackBoard" + (toInt(nowPosPlace[nowPosPlace.Length - 1]) - 1).ToString();
 rules[7] = (toInt(nowPosPlace[0]) - 1).ToString() + "blackBoard" + (toInt(nowPosPlace[nowPosPlace.Length - 1]) + 1).ToString();
 
-rules[7] = (toInt(nowPosPlace[0]) - 2).ToString() + "blackBoard" + (toInt(nowPosPlace[nowPosPlace.Length - 1]) - 1).ToString();
-rules[8] = (toInt(nowPosPlace[0]) - 2).ToString() + "blackBoard" + (toInt(nowPosPlace[nowPosPlace.Length - 1]) + 1).ToString();
-
-
-       
+rules[8] = (toInt(nowPosPlace[0]) - 2).ToString() + "blackBoard" + (toInt(nowPosPlace[nowPosPlace.Length - 1]) - 1).ToString();
+rules[9] = (toInt(nowPosPlace[0]) - 2).ToString() + "blackBoard" + (toInt(nowPosPlace[nowPosPlace.Length - 1]) + 1).ToString();       
 }
 
 public void Board() {
@@ -150,15 +153,13 @@ public void Board() {
                  getPosition();   
 
 setRules();
- if(MoveRules(nameOf)) {
+if(MoveRules(nameOf)) {
 
         if(nameOf.Contains("redPiece")) {
-       if (CheckSameCollision(redPieces)) {
+         if (CheckSameCollision(redPieces)) {
          piece.transform.position = board.transform.Find(positionName).transform.position;
-      
+         }
       }
-
-        }
          if(nameOf.Contains("blackPiece")) {
    if (CheckSameCollision(blackPieces)) {
          piece.transform.position = board.transform.Find(positionName).transform.position;
@@ -167,7 +168,7 @@ setRules();
  }
 
 
-//for Red piece
+
 }
 }
    }
