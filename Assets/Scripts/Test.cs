a19b321c578eca8dbab4de289e466fd68efc4037
Michaela Ř <majkeltcz@gmail.com>
2023-12-28 20:13:50 +0100

    dokumentace přílohy

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,11 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+
+
 
 public class Test: MonoBehaviour
 {
-    // Start is called before the first frame update
+     [SerializeField] 
+   ARRaycastManager RaycastManager; 
+   ARPlaneManager m_PlaneManager;
+
+   List<ARRaycastHit> hits = new List<ARRaycastHit>();
  public void Move() {
-    transform.position = new Vector3(0.1f, 0, 0);
+
+  if (RaycastManager.Raycast(Input.GetTouch(0).position, hits)) { 
+
+
+    if(Input.GetTouch(0).phase == TouchPhase.Began) { 
+     transform.position =   hits[0].pose.position; 
+       
+        } 
+    
+         }   
 }
+
+
+
+
+
+
+
+
+   // transform.position = new Vector3(0.1f, 0, 0);
+
 }
